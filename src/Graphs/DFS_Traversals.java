e21a7ca9fb4fb79a40571baf38b6862f795e50be
konpeeyush <piyushksingh2589@gmail.com>
2022-04-22 14:56:52 +0530

    Adding Graph DFS

---
+++ src/Graphs/DFS_Traversals.java
@@ -0,0 +1,22 @@
+//package Graphs;
+//
+//import java.util.List;
+//import java.util.Set;
+//
+//public class DFS_Traversals {
+//    public void DFS(int source, Set<Integer> vis){
+//        System.out.println(source);
+//        vis.add(source);
+//        List<Integer> neighbourList=gobj.get(source) ;
+//        for (int neighbour:neighbourList
+//             ) {
+//            if(!vis.contains(neighbour)){
+//                DFS(neighbour,vis) ;
+//            }
+//        }
+//    }
+//    public static void main(String[] args) {
+//
+//    }
+//}
+
--- src/Graphs/DFS_Travesrsals.java
+++
@@ -1,7 +0,0 @@
-package Graphs;
-
-public class DFS_Travesrsals {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
