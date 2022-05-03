65c567be7af95008dedd7fc3edf160e964ebbfe2
konpeeyush <piyushksingh2589@gmail.com>
2022-05-03 18:36:14 +0530

    Correcting the code.

--- src/Sorting/Selection_Sort.java
+++ src/Sorting/Selection_Sort.java
@@ -5,25 +5,21 @@ import java.util.Arrays;
 
 public class Selection_Sort {
     public static void main(String[] args) {
-        int[] arr={20,10} ;
-        Selection_Sort f=new Selection_Sort() ;
-        f.selection(arr);
+        int[] arr={50,40,30,10,100} ;
+        selection(arr);
         System.out.println(Arrays.toString(arr));
     }
-    void selection(int[] arr){
-        for (int i = 0; i < arr.length-1; i++) {
-            int min=i ;
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[j]<arr[min]){
-                    min=j ;
-                }
-                if(min!=i)
-                {
-                    int temp=arr[min] ;
-                    arr[min]=arr[i] ;
-                    arr[i]=temp ;
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
                 }
             }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
         }
     }
 }
